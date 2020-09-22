e810194e166193de8ba12f887ccdde2f5f4ac74e
fecampi <felipe.campinho@hotmail.com>
2020-09-22 07:22:03 -0300

    first commit

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  jwt: {
+    // http://www.md5.cz/
+    secret: '54e4d4b9b8a6b721d245fb7841bcc75c',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('this appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
